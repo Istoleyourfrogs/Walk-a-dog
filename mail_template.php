9aabb052361106014782529c8861869cc48b83b7
Stefan Gasparic <car.stefan@ymail.com>
2019-01-03 22:57:26 +0100

    newsletter mail template

--- mail_template.php
+++ mail_template.php
@@ -44,17 +44,12 @@
         div[style*="margin: 16px 0;"] { margin: 0 !important; }
     </style>
 </head>
-<body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
-
-<!-- HIDDEN PREHEADER TEXT -->
-<div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
-    We're thrilled to have you here! Get ready to dive into your new account.
-</div>
+<body style="background-color: #51e5ff; margin: 0 !important; padding: 0 !important;">
 
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <!-- LOGO -->
     <tr>
-        <td bgcolor="#51E5FF" align="center">
+        <td bgcolor="#ffd6c0" align="center">
             <!--[if (gte mso 9)|(IE)]>
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
                 <tr>
@@ -63,8 +58,8 @@
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                 <tr>
                     <td align="center" valign="top" style="padding: 40px 10px 40px 10px;">
-                        <a href="http://litmus.com" target="_blank">
-                            <img alt="Logo" src="images/dog.svg" width="250" height="125" style="display: block; color: #ffffff; font-size: 18px;" border="0">
+                        <a href="https://walkadog.secondsection.in.rs/index.php" target="_blank">
+                            <img alt="Logo" src="images/dog.svg" style="display: block; width: 400px; max-width: 400px; min-width: 400px;   color: #ffffff; font-size: 18px;" border="0">
                         </a>
                     </td>
                 </tr>
@@ -78,7 +73,7 @@
     </tr>
     <!-- HERO -->
     <tr>
-        <td bgcolor="#51E5FF" align="center" style="padding: 0px 10px 0px 10px;">
+        <td bgcolor="#ffd6c0" align="center" style="padding: 0px 10px 0px 10px;">
             <!--[if (gte mso 9)|(IE)]>
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
                 <tr>
@@ -86,8 +81,8 @@
             <![endif]-->
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                 <tr>
-                    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111;   font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                        <h1 style="font-size: 48px; font-weight: 400; margin: 0;">Welcome!</h1>
+                    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #ec368d;   font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                        <h1 style="font-size: 48px; font-weight: 400; color: #ec368d; margin: 0;">Welcome!</h1>
                     </td>
                 </tr>
             </table>
@@ -100,7 +95,7 @@
     </tr>
     <!-- COPY BLOCK -->
     <tr>
-        <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+        <td bgcolor=#ffa5a5 align="center" style="padding: 0px 10px 0px 10px;">
             <!--[if (gte mso 9)|(IE)]>
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
                 <tr>
@@ -109,11 +104,14 @@
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                 <!-- COPY -->
                 <tr>
-                    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
-                        <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
+                    <td bgcolor="#ffffff" align="center" style="padding: 20px 40px 30px 40px; color: #ec368d;   font-size: 40px; font-weight: 400; line-height: 25px; border-radius: 4px 4px 4px 4px;" >
+                        <p style="margin: 0; font-size: 18px;">Thank you for signing up for our newsletter service. We are happy to have you here!</p>
                     </td>
                 </tr>
-                <!-- BULLETPROOF BUTTON -->
+                <tr>
+                    <td><br></td>
+                </tr>
+                <!-- BULLETPROOF BUTTON - ovo nije potrebno kod newsletter mail-a
                 <tr>
                     <td bgcolor="#ffffff" align="left">
                         <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -121,38 +119,38 @@
                                 <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                     <table border="0" cellspacing="0" cellpadding="0">
                                         <tr>
-                                            <td align="center" style="border-radius: 3px;" bgcolor="#EC368D"><a href="https://litmus.com" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #EC368D; display: inline-block;">Confirm Account</a></td>
+                                            <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href="https://litmus.com" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirm Account</a></td>
                                         </tr>
                                     </table>
                                 </td>
                             </tr>
                         </table>
                     </td>
-                </tr>
-                <!-- COPY -->
+                </tr>-->
+                <!-- COPY
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
                         <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
                     </td>
                 </tr>
-                <!-- COPY -->
+                <!-- COPY
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 20px 30px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
-                        <p style="margin: 0;"><a href="http://litmus.com" target="_blank" style="color: #EC368D;">XXX.XXXXXXX.XXX/XXXXXXXXXXXXX</a></p>
+                        <p style="margin: 0;"><a href="http://litmus.com" target="_blank" style="color: #FFA73B;">XXX.XXXXXXX.XXX/XXXXXXXXXXXXX</a></p>
                     </td>
                 </tr>
-                <!-- COPY -->
+                <!-- COPY
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
                         <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to help out.</p>
                     </td>
                 </tr>
-                <!-- COPY -->
+                <!-- COPY
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
                         <p style="margin: 0;">Cheers,<br>The Ceej Team</p>
                     </td>
-                </tr>
+                </tr>-->
             </table>
             <!--[if (gte mso 9)|(IE)]>
             </td>
@@ -163,7 +161,7 @@
     </tr>
     <!-- SUPPORT CALLOUT -->
     <tr>
-        <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
+        <td bgcolor="#51e5ff" align="center" style="padding: 30px 10px 50px 10px;">
             <!--[if (gte mso 9)|(IE)]>
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
                 <tr>
@@ -172,10 +170,14 @@
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                 <!-- HEADLINE -->
                 <tr>
-                    <td bgcolor="#FFA5A5" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666;   font-size: 18px; font-weight: 400; line-height: 25px;" >
-                        <h2 style="font-size: 20px; font-weight: 400; color: #fff; margin: 0;">Need more help?</h2>
-                        <p style="margin: 0;"><a href="http://litmus.com" target="_blank" style="color: #fff;">We&rsquo;re here, ready to talk</a></p>
+                    <td bgcolor="#ec368d" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px;  font-size: 18px; font-weight: 400; line-height: 25px;" >
+                        <h2 style="font-size: 24px; font-weight: 400; color: #ffffff; margin: 0; text-transform: none;">Got a question for us? </h2>
+                        <p style="margin: 0; font-size: 30px; font-weight: 700;"><a href="https://walkadog.secondsection.in.rs/index.php#contact" target="_blank" style="color: #ffffff;">We&rsquo;re here, ready to talk</a></p>
+                        <br>
+                        <!-- UNSUBSCRIBE -->
+                        <p style="margin: 0; color: #ffffff; font-size:16px">if these emails get annoying, please feel free to <a href="http://litmus.com" target="_blank" style="color: #ffffff; font-weight: 700;">unsubscribe</a>.</p>
                     </td>
+
                 </tr>
             </table>
             <!--[if (gte mso 9)|(IE)]>
@@ -185,51 +187,7 @@
             <![endif]-->
         </td>
     </tr>
-    <!-- FOOTER -->
-    <tr>
-        <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
-            <!--[if (gte mso 9)|(IE)]>
-            <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                <tr>
-                    <td align="center" valign="top" width="600">
-            <![endif]-->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
-                <!-- NAVIGATION -->
-                <tr>
-                    <td bgcolor="#f4f4f4" align="left" style="padding: 30px 30px 30px 30px; color: #666666;   font-size: 14px; font-weight: 400; line-height: 18px;" >
-                        <p style="margin: 0;">
-                            <a href="http://litmus.com" target="_blank" style="color: #111111; font-weight: 700;">Dashboard</a> -
-                            <a href="http://litmus.com" target="_blank" style="color: #111111; font-weight: 700;">Billing</a> -
-                            <a href="http://litmus.com" target="_blank" style="color: #111111; font-weight: 700;">Help</a>
-                        </p>
-                    </td>
-                </tr>
-                <!-- PERMISSION REMINDER -->
-                <tr>
-                    <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666;   font-size: 14px; font-weight: 400; line-height: 18px;" >
-                        <p style="margin: 0;">You received this email because you just signed up for a new account. If it looks weird, <a href="http://litmus.com" target="_blank" style="color: #111111; font-weight: 700;">view it in your browser</a>.</p>
-                    </td>
-                </tr>
-                <!-- UNSUBSCRIBE -->
-                <tr>
-                    <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666;   font-size: 14px; font-weight: 400; line-height: 18px;" >
-                        <p style="margin: 0;">If these emails get annoying, please feel free to <a href="http://litmus.com" target="_blank" style="color: #111111; font-weight: 700;">unsubscribe</a>.</p>
-                    </td>
-                </tr>
-                <!-- ADDRESS -->
-                <tr>
-                    <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666;   font-size: 14px; font-weight: 400; line-height: 18px;" >
-                        <p style="margin: 0;">Ceej - 1234 Main Street - Anywhere, MA - 56789</p>
-                    </td>
-                </tr>
-            </table>
-            <!--[if (gte mso 9)|(IE)]>
-            </td>
-            </tr>
-            </table>
-            <![endif]-->
-        </td>
-    </tr>
+
 </table>
 
 </body>
