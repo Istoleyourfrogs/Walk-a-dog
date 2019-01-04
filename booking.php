5faad34f2fc9e09e375bf24dec01d8353df48bea
Luka <patarcic98@gmail.com>
2019-01-04 23:53:24 +0100

    hehe promene smesno :FeelsSmesnoMan:

--- booking.php
+++ booking.php
@@ -19,100 +19,115 @@ require "includes/header.inc.php";
             <div class="row">
                 <div class="contact-page-inner">
 
-                    <div class="col-md-12 contact-right-form">
+                    <div class="col-sm-12 contact-right-form">
                         <div class="contact-page-title">
                             <h4>Enter you information and something something something</h4>
                         </div>
                             <div class="row">
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label>First Name:</label>
-                                        <input type="text" class="form-control" placeholder="name">
+                                        <input type="text" class="form-control" placeholder="First Name" name="firstName">
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label>Last Name:</label>
-                                        <input type="text"  class="form-control" placeholder="email">
+                                        <input type="text"  class="form-control" placeholder="Last Name" name="lastName">
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label>Email:</label>
-                                        <input type="text" class="form-control" placeholder="name">
+                                        <input type="text" class="form-control" placeholder="email" name="email">
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label>Address:</label>
-                                        <input type="text" class="form-control" placeholder="name">
+                                        <input type="text" class="form-control" placeholder="Address" name="address">
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label>Phone Number:</label>
-                                        <input type="text"  class="form-control" placeholder="email">
+                                        <input type="text"  class="form-control" placeholder="Phone Number" name="phone">
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="typeWalk">Type of walk:</label>
                                         <select id="typeWalk" name="typeWalk" class="form-control">
-                                            <option>Select the type of walk</option>
-                                            <option>One time</option>
-                                            <option>Daily</option>
-                                            <option>Weekly</option>
+                                            <option value="">Select the type of walk</option>
+                                            <option value="oneTime">One time</option>
+                                            <option value="daily">Daily</option>
+                                            <option value="weekly">Weekly</option>
                                         </select>
                                     </div>
                                 </div>
                                 </div>
                             <div class="row">
-
-                                <div class="col-md-2 none" walk="date">
+                                <div class="col-sm-2 none" walk="date">
                                     <div class="form-group">
-                                        <label for="numberOfDogs">Date of walk:</label>
-                                        <input type="date" name="date">
+                                        <label for="date">Date of walk:</label>
+                                        <input id="date" class="form-control" type="date" name="date">
                                     </div>
                                 </div>
-                                <div class="col-md-2 none" walk="time">
+                                <div class="col-sm-2 none" walk="time">
                                     <div class="form-group">
                                         <label for="time">Time of walk:</label>
-                                        <input type="time" name="time">
+                                        <input id="time" class="form-control" type="time" name="time">
                                     </div>
                                 </div>
-                                <div class="col-md-8 none" walk="day">
-                                    <div class="form-group">
+                                <div class="col-sm-10 none" walk="day">
+                                    <div class="btn-group" data-toggle="buttons">
                                         <label>Which day: </label><br>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Monday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Tuesday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Wednesday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Thursday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Friday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Saturday</label>
-                                        <input type="radio"  class="" placeholder="email">
-                                        <label>Sunday</label>
+                                        <label for="monday" class="btn btn-primary">Monday
+                                        <input id="monday" type="radio" name="day" value="monday" class="">
+                                        </label>
+                                        <label for="tuesday" class="btn btn-primary">Tuesday
+                                        <input id="tuesday" type="radio"   name="day" value="tuesday" class="">
+                                        </label>
+                                        <label for="wednesday" class="btn btn-primary">Wednesday
+                                        <input id="wednesday" type="radio" name="day" value="wednesday" class="">
+                                        </label>
+                                        <label for="thursday" class="btn btn-primary">Thursday
+                                        <input id="thursday" type="radio"   name="day" value="thursday">
+                                        </label>
+                                        <label for="friday" class="btn btn-primary">Friday
+                                        <input id="friday" type="radio"   name="day" value="friday">
+                                        </label>
+                                        <label for="saturday" class="btn btn-primary">Saturday
+                                        <input id="saturday" type="radio"   name="day" value="saturday">
+                                        </label>
+                                        <label for="sunday" class="btn btn-primary">Sunday
+                                        <input id="sunday" type="radio"  name="day" value="sunday">
+                                        </label>
                                     </div>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="numberOfDogs">Number of Dogs:</label>
-                                        <select id="numberOfDogs" class="form-control" name="numberOfDogs">
-                                            <option  value="0">Number of Dogs</option>
-                                            <?php
-                                            for($i=1; $i<=3; $i++){
-                                                echo "<option id=\"{$i}dog\" value=\"$i\" name=\"numberOfDogs\">$i</option>";
-                                            }
-                                            ?>
-                                        </select>
-                                    </div>
+
+                            </div>
+                            <div class="row">
+                            <div class="col-sm-4">
+                                <div class="form-group">
+                                    <label for="numberOfDogs">Number of Dogs:</label>
+                                    <select id="numberOfDogs" class="form-control" name="numberOfDogs">
+                                        <option  value="0">Number of Dogs</option>
+                                        <?php
+                                        for($i=1; $i<=3; $i++){
+                                            echo "<option id=\"{$i}dog\" value=\"$i\" name=\"numberOfDogs\">$i</option>";
+                                        }
+                                        ?>
+                                    </select>
                                 </div>
                             </div>
+
+                            <div class="col-sm-8 pt-5" align="center">
+                                <img class="mr-5" src="images/cute-dog.svg" alt="dog" width="100">
+                                <img class="mr-5" src="images/cute-dog.svg" alt="dog" width="100">
+                                <img class="mr-5" src="images/cute-dog.svg" alt="dog" width="100">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -124,87 +139,76 @@ require "includes/header.inc.php";
             <div class="row">
                 <div class="contact-page-inner">
 
-                    <div class="col-md-12 contact-right-form">
+                    <div class="col-sm-12 contact-right-form">
                         <div class="contact-page-title">
                             <h4>Enter your dogs information below</h4>
                         </div>
                             <div id="dog" class="row">
-                                <div class="col-md-4 none" dog="1">
+                                <div class="col-sm-4 none" dog="1">
                                     <h4>First dog</h4>
                                     <hr>
                                     <div class="form-group ">
                                         <label>Name First</label>
                                         <input type="text" class="form-control" placeholder="name first">
                                     </div>
-
                                     <div class="form-group ">
                                         <label>Age First</label>
                                         <input type="text" class="form-control" placeholder="name first">
                                     </div>
-
-
-
                                     <div class="form-group ">
                                         <label>Breed First</label>
                                         <input type="text" class="form-control" placeholder="name first">
                                     </div>
-
-
-                                    <div class="form-group ">
+                                    <div class="btn-group-vertical mb-4 form-group" data-toggle="buttons">
                                         <label class="headingDog">Vaccinated First</label><br>
-                                        <label for="vacYes1">YES</label>
+                                        <label class="btn btn-primary" for="vacYes1">YES
                                         <input type="radio" id="vacYes1" name="vac1"><br>
-                                        <label for="vacNo1">NO</label>
+                                        </label>
+                                        <label class="btn btn-primary" for="vacNo1">NO
                                         <input type="radio" id="vacNo1" name="vac1"><br>
+                                        </label>
                                     </div>
-
-
-
-                                    <div class="form-group ">
+                                    <div class="btn-group-vertical mb-4 form-group" data-toggle="buttons">
                                         <label class="headingDog">Trained First</label><br>
-                                        <label for="t11">Not agressive</label>
+                                        <label class="btn btn-primary" for="t11">Not agressive
                                         <input type="radio" id="t11" name="trained1"><br>
-                                        <label for="t12">Little agressive</label>
+                                        </label>
+                                        <label class="btn btn-primary" for="t12">Little agressive
                                         <input type="radio" id="t12" name="trained1"><br>
-                                        <label for="t13">Agressive</label>
+                                        </label>
+                                        <label class="btn btn-primary" for="t13">Agressive
                                         <input type="radio" id="t13" name="trained1">
+                                        </label>
                                     </div>
-
-                                    <div class="form-group ">
+                                    <div class="btn-group-vertical mb-4 form-group" data-toggle="buttons">
                                         <label class="headingDog">Agression First</label><br>
-                                        <label for="ag11">Not agressive</label>
+                                        <label class="btn btn-primary" for="ag11">Not agressive
                                         <input type="radio" id="ag11" name="agression1"><br>
-                                        <label for="ag12">Little agressive</label>
+                                        </label>
+                                        <label class="btn btn-primary" for="ag12">Little agressive
                                         <input type="radio" id="ag12" name="agression1"><br>
-                                        <label for="ag13">Agressive</label>
+                                        </label>
+                                        <label class="btn btn-primary" for="ag13">Agressive
                                         <input type="radio" id="ag13" name="agression1">
+                                        </label>
                                     </div>
-
-
                                     <div class="form-group ">
                                         <label class="headingDog">Other First</label>
                                         <textarea placeholder="Enter text here" rows="5"></textarea>
                                     </div>
                                     <hr>
                                 </div>
-
-
-
-                                <div class="col-md-4 none" dog="2">
+                                <div class="col-sm-4 none" dog="2">
                                     <h4>Second dog</h4>
                                     <hr>
                                     <div class="form-group ">
                                         <label>Name First</label>
                                         <input type="text" class="form-control" placeholder="name first">
                                     </div>
-
                                     <div class="form-group ">
                                         <label>Age First</label>
                                         <input type="text" class="form-control" placeholder="name first">
                                     </div>
-
-
-
                                     <div class="form-group ">
                                         <label>Breed First</label>
                                         <input type="text" class="form-control" placeholder="name first">
@@ -249,7 +253,7 @@ require "includes/header.inc.php";
                                     <hr>
                                 </div>
 
-                                <div class="col-md-4 none" dog="3">
+                                <div class="col-sm-4 none" dog="3">
                                     <h4>Third dog</h4>
                                     <hr>
                                     <div class="form-group ">
@@ -316,7 +320,6 @@ require "includes/header.inc.php";
     </div>
 </form>      <!-- DOG FORM SECTION END-->
 
-
 <!-- js library start -->
 <script  src="js/jquery-3.2.1.min.js"></script>
 <script  src="js/bootstrap.min.js"></script>
@@ -374,26 +377,35 @@ $(document).ready(function () {
         var time = $('div[walk="time"]');
         var day = $('div[walk="day"]');
 
-        if(valueSelect2.val() === 'One time'){
+        if(valueSelect2.val() === ''){
+            date.addClass("none");
+            time.addClass("none");
+            day.addClass("none");
+            date.addClass("none");
+        }
+        if(valueSelect2.val() === 'oneTime'){
             date.removeClass("none");
             time.removeClass("none");
             day.addClass("none");
             date.removeClass("none");
         }
-        if(valueSelect2.val() === 'Daily'){
+        if(valueSelect2.val() === 'daily'){
             date.removeClass("none");
             time.removeClass("none");
             day.addClass("none");
             date.addClass("none");
         }
-        if(valueSelect2.val() === 'Weekly'){
+        if(valueSelect2.val() === 'weekly'){
             date.removeClass("none");
             time.removeClass("none");
             day.removeClass("none");
             date.addClass("none");
+
         }
 
-})
+    });
+
+
 
 }) ;
 </script>
---
+++ includes/booking.inc.php
@@ -0,0 +1,10 @@
+<?php
+$typeOfWalk = $_POST['typeWalk'];
+$dateOfWalk = $_POST['date'];
+$timeOfWalk = $_POST['time'];
+$dayOfWalk = $_POST['day'];
+
+echo "type of walk: $typeOfWalk <br>";
+echo "date of walk: $dateOfWalk <br>";
+echo "time of walk: $timeOfWalk <br>";
+echo "day of walk: $dayOfWalk <br>";
